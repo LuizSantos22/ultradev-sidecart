388d730bc9509088383d1bae0705a099faf69133
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-04-07 15:43:26 -0300

    Add Data helper class for SideCart functionality

--- app/code/community/UltraDev/SideCart/Helper/Data.php
+++ app/code/community/UltraDev/SideCart/Helper/Data.php
@@ -1,1 +1,21 @@
 
+<?php
+class UltraDev_SideCart_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function isEnabled()
+    {
+        return (bool) Mage::getStoreConfig('ultradev_sidecart/general/enabled');
+    }
+
+    public function getCartSelector()
+    {
+        $selector = Mage::getStoreConfig('ultradev_sidecart/general/cart_selector');
+        return $selector ?: '.top-link-cart';
+    }
+
+    public function getAddToCartSelector()
+    {
+        $selector = Mage::getStoreConfig('ultradev_sidecart/general/addtocart_selector');
+        return $selector ?: '.btn-cart, .actions .btn-cart';
+    }
+}
